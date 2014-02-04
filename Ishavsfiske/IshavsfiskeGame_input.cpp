6555c810dbce85f40b496fde5fcf0d777d4150c0
Ishavsfiske <a13sihli@student.his.se>
2014-02-04 12:12:23 +0100

    Added wasd-key inputs to move and rotate ship (v0.1.2.a22)

--- Ishavsfiske/IshavsfiskeGame_input.cpp
+++ Ishavsfiske/IshavsfiskeGame_input.cpp
@@ -1,5 +1,5 @@
 //Version: 0.1.2
-//Author: 
+//Author: Sihao Li
 //Contributors: 
 
 #ifndef ISHAV_0_1_2
@@ -14,5 +14,20 @@
 
 void Ishavsfiske::IshavsfiskeGame::mInput(float time, float deltaTime)
 {
-	
+	if (getKeyboardState().isKeyDown(sf::Keyboard::W))
+	{
+		mShip->move(0, -0.5 * deltaTime);
+	}
+	if (getKeyboardState().isKeyDown(sf::Keyboard::A))
+	{
+		mShip->rotate(-90 * deltaTime);
+	}
+	if (getKeyboardState().isKeyDown(sf::Keyboard::S))
+	{
+		mShip->move(0, 0.5 * deltaTime);
+	}
+	if (getKeyboardState().isKeyDown(sf::Keyboard::D))
+	{
+		mShip->rotate(90 * deltaTime);
+	}
 }
