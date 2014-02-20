e5e51557062c4203b65c725b79a2498dc3381ec2
thenohatcat <jakobia_2@hotmail.com>
2014-02-20 14:13:57 +0100

    Added fishing shape (v0.1.4.a1)

--- Ishavsfiske/FishingBoat.cpp
+++ Ishavsfiske/FishingBoat.cpp
@@ -55,21 +55,24 @@ void FishingBoat::mInit()
 	pts.push_back(sf::Vector2f(1.5f/2.5f*(-0.5f + (45 / 75.0f)), -0.5f + (2 / 125.0f)));
 	new Angler::Nodes::CollisionNode(getID() + 0x0102, s, pts, 0);
 
-	Angler::Nodes::Translation *rightFishT = new Angler::Nodes::Translation(getID() + 0x0200, s, 0.5, 0);
 	std::vector<sf::Vector2f> fishPTS;
 	for (int i = 0; i < 16; i++)
 	{
-		fishPTS.push_back(sf::Vector2f(1/3.0f*cos(i/16.0f * 2*3.14159f), sin(i/16.0f * 2*3.14159f)));
+		fishPTS.push_back(sf::Vector2f(1/4.0f*cos(i/16.0f * 2*3.14159f), 0.5f*sin(i/16.0f * 2*3.14159f)));
 	}
 	//new Angler::Nodes::CollisionNode(getID() + 0x0201, rightFishT, fishPTS, 0);
-	//new Angler::Nodes::CollisionNode(0x0201, rightFishT, fishPTS, 0);
+	Angler::Nodes::Translation *rightFishT = new Angler::Nodes::Translation(getID() + 0x0200, s, 0.45f, 0.15f);
+	new Angler::Nodes::CollisionNode(0x0201, rightFishT, fishPTS, 0);
+
+	Angler::Nodes::Translation *leftFishT = new Angler::Nodes::Translation(getID() + 0x0200, s, -0.45f, 0.15f);
+	new Angler::Nodes::CollisionNode(0x0202, leftFishT, fishPTS, 0);
 
 	Angler::Nodes::Translation *craneT = new Angler::Nodes::Translation(getID() + 0x0103, mShipRoot, 
 		2.5f/20.0f * 1.5f/2.5f*(-0.5f+0.506f), 2.5f/20.0f * (-0.5+0.672f));
 	Angler::Nodes::Scale *craneS = new Angler::Nodes::Scale(getID() + 0x0104, craneT, 14/1000.0f, 14/1000.0f);
 	mCraneRotation = new Angler::Nodes::Rotation(getID() + 0x0105, craneS, 0);
 	//new Angler::Nodes::DrawNode(getID() + 0x0106, mCraneRotation, 2, 2.0f/3.0f*0.075f, 0.5);
-	new Angler::Nodes::SpriteNode(getID() + 0x0106, mCraneRotation, 2, sf::Vector2f(0, 0), sf::Vector2f(560/1500.0f, 318/1600.0f), sf::Vector2f(93/1500.0f, 14/1600.0f));
+	new Angler::Nodes::SpriteNode(getID() + 0x0106, mCraneRotation, 2, sf::Vector2f(2.0f/3.0f*0.075f, 0.5), sf::Vector2f(560/1500.0f, 318/1600.0f), sf::Vector2f(93/1500.0f, 14/1600.0f));
 
 	Angler::Nodes::Translation *lampT = new Angler::Nodes::Translation(getID() + 0x0103, mShipRoot, 
 		2.5f/20.0f * 1.5f/2.5f * (-0.5f+0.586f), 2.5f/20.0f * (-0.5f+0.304f));
@@ -81,7 +84,7 @@ void FishingBoat::mInit()
 
 void FishingBoat::update(Angler::Game *context, float time, float deltaTime, bool changed)
 {
-	mCraneRotation->setRotation(90-45*sin(time));
+	mCraneRotation->setRotation(45/2.0f*sin(time));
 	mLampRotation->setRotation(90*sin(time));
 
 	Ship::update(context, time, deltaTime, changed);
